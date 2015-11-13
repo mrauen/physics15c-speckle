d63b12471e278ab0265dc00f3898b8750119ecf5
Matthew Rauen <rauen@college.harvard.edu>
2015-11-13 18:59:47 -0500

    Last attempt at using Matlab

--- speckle.m
+++ speckle.m
@@ -1,7 +1,7 @@
 % Read in the images
-image1 = double(imread(''));
-image2 = double(imread(''));
-image3 = double(imread(''));
+image1 = double(rgb2gray(imread('images/DSC_0115.jpg')));
+image2 = double(rgb2gray(imread('images/DSC_0116.jpg')));
+image3 = double(rgb2gray(imread('images/DSC_0117.jpg')));
 
 % Normalize the intensities to be in (-1, 1)
 normalizedimage1 = (image1 ./ 128) - 1;
@@ -9,4 +9,5 @@ normalizedimage2 = (image2 ./ 128) - 1;
 normalizedimage3 = (image3 ./ 128) - 1;
 
 % Get the array of phases
-phaseArray = arrayfun(computePhase, normalizedimage1, normalizedimage2, normalizedimage3);
+phaseArray = arrayfun(@(x, y, z) arrayfun(computePhase, x, y, z), normalizedimage1, normalizedimage2, normalizedimage3);
+imshow(phaseArray);
